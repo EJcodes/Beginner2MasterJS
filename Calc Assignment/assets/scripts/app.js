d13e3cd5447c0b3e74a57d581c1ec4dc34a9039f
EJcodes <IHMDemon.exe@gmail.com>
2019-11-13 12:41:13 -0500

    refactored the code and began the logic for subtraction

--- Calc Assignment/assets/scripts/app.js
+++ Calc Assignment/assets/scripts/app.js
@@ -2,15 +2,35 @@ const defaultResult = 0;
 let currentResult = defaultResult;
 
 function getUserNumberInput() {
-    
     return parseInt(userInput.value);
 }
-function add() {
-    const enteredNumber = parseInt(userInput.value);
-    const calcDescription = `${currentResult} + ${enteredNumber}`
-    currentResult = currentResult + enteredNumber;
-    outputResult(currentResult, calcDescription);
+
+function createWriteOutput(operator, resultBeforeCalc, calcNumber) {
+    const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+    outputResult(currentResult, calcDescription)
 }
+
+function add() {
+    const enteredNumber = getUserNumberInput();
+    initialResult = currentResult;
+    currentResult = currentResult + enteredNumber;
+    createWriteOutput('+', initialResult, enteredNumber );
+}
+
+function subtract(){
+    const enteredNumber = paseInt(userInput.value);
+    currentResult = currentResult - enteredNumber;
+    createWriteOutput('-', initialResult, enteredNumber);
+}
+
+function multiply(){
+
+}
+
+function divide(){
+
+}
+
 
 addBtn.addEventListener('click', add);
 
