b26de3be6ff398502c681151677ab40b2476e2b0
EJcodes <IHMDemon.exe@gmail.com>
2019-11-12 19:19:54 -0500

    finished assignment 2

--- AssignmentTwo/assignment.js
+++ AssignmentTwo/assignment.js
@@ -1,1 +1,19 @@
 const task3Element = document.getElementById('task-3');
+
+function anAlert() {
+    alert( "blah blah");
+}
+
+function nameCaller(name) {
+    alert(`Yoooooo ${name}`);
+}
+
+nameCaller('Evan');
+threeNames('Mari', 'Lisa', 'Ashely');
+
+task3Element.addEventListener('click', anAlert);
+
+function threeNames(name1, name2, name3) {
+    return alert(`name1: ${name1}, name2: ${name2}, name3: ${name3}`)
+}
+
